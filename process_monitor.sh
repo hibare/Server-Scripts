596d411e9292d122ceec780d0836a1840986c63f
Shubham Hibare <20609766+hibare@users.noreply.github.com>
2020-12-26 13:59:43 +0530

    Remove decomissioned gotify webhook

--- process_monitor.sh
+++ process_monitor.sh
@@ -8,5 +8,4 @@ else
 	echo " Process stopped"
 	mega-help
 	echo "Started process"
-        /usr/bin/curl -X POST -s -F "title=Mega" -F "message=Mega Server Process restart" -F "priority=5" "https://notify.hibare.in/message?token=AT1GLiV2uy6Jx8n"
 fi
