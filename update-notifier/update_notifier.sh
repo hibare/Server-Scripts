7f9d8a3a17ce0f40e2a728b44c06547f3e7075e2
Shubham Hibare <github@hibare.in>
2022-09-03 10:47:42 +0530

    Add update notifier

---
+++ update-notifier/update_notifier.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+message=""
+server="$(hostname -f)"
+
+IFS=';' read updates security_updates < <(/usr/lib/update-notifier/apt-check 2>&1)
+
+if [ $updates -gt 0 ]
+then
+    message="${message}${updates} updates can be applied immediately."
+fi
+
+if [ $security_updates -gt 0 ]
+then
+    message="${message}\n${security_updates} of these updates are standard security updates."
+fi
+
+if [ ! -z "$message"  ]
+then
+    curl --request POST \
+        --url https://discord.com/api/webhooks/866714081385119835/MqgvnL4NQnElpU8Nt9BRJRbnggDZs3o-aaM0A5_DNZKc3NilsmNvYBLrjPh27J3ithpu \
+        --header 'Content-Type: application/json' \
+        --cookie '__dcfduid=02a5cd2e1b2f11ed8765468f320d6606; __sdcfduid=02a5cd2e1b2f11ed8765468f320d6606797678dd30562f3264189e08596be9d00b508cd40758e24373b2e5709368bb99; __cfruid=00e54275ec73526a836ce2134b7f86f109a360ec-1661773699' \
+        --data '{
+        "username": "Server Notifications",
+        "avatar_url": "https://i.imgur.com/4M34hi2.png",
+        "embeds": [
+            {
+            "title": ":gear: Updates Available",
+            "color": 16248591,
+            "fields": [
+                {
+                "name": "Server",
+                "value": "'"$server"'"
+                }
+            ],
+                    "description": "```'"$message"'```"
+            }
+        ]
+        }
+        '
+fi
