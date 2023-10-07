3870bf886e8a7322072c3b027b75982afca765c9
Shubham Hibare <shubham@hibare.in>
2023-10-07 22:23:09 +0530

    Add directory skip

--- scripts/docker-stack-update/docker_stack_update.sh
+++ scripts/docker-stack-update/docker_stack_update.sh
@@ -3,11 +3,29 @@
 # Define the directory where your Docker Compose stacks are located
 DOCKER_DIR="$HOME/docker"
 
+# Array of directory names to skip
+declare -a dirs_to_skip=("portainer")
+
 # List all subdirectories in the Docker directory
 for stack_dir in "$DOCKER_DIR"/*/; do
     
-    if [ -f "$stack_dir/docker-compose.yml" ]; then
+    if [ -d "$stack_dir" ] && [ -f "$stack_dir/docker-compose.yml" ]; then
         stack_name=$(basename "$stack_dir")
+        
+        # Check if the current directory's name is in the list of directories to skip
+        skip_directory=false
+        for dir_to_skip in "${dirs_to_skip[@]}"; do
+            if [ "$stack_name" == "$dir_to_skip" ]; then
+                echo "Skipping directory $stack_name."
+                skip_directory=true
+                break
+            fi
+        done
+
+        # If the directory is marked for skipping, continue to the next one
+        if [ "$skip_directory" == "true" ]; then
+            continue
+        fi
 
         echo "Checking stack $stack_name"
 
