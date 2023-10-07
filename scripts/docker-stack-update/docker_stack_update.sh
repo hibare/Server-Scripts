329868b30cf3040f126d4ef6c7e7c64cc1f22736
Shubham Hibare <shubham@hibare.in>
2023-10-07 22:16:18 +0530

    Fix commands

--- scripts/docker-stack-update/docker_stack_update.sh
+++ scripts/docker-stack-update/docker_stack_update.sh
@@ -5,29 +5,32 @@ DOCKER_DIR="$HOME/docker"
 
 # List all subdirectories in the Docker directory
 for stack_dir in "$DOCKER_DIR"/*/; do
+    
     if [ -f "$stack_dir/docker-compose.yml" ]; then
         stack_name=$(basename "$stack_dir")
 
+        echo "Checking stack $stack_name"
+
         # Get a list of running service names
-        running_services=$(docker-compose -f "$stack_dir/docker-compose.yml" ps --services)
+        running_services=$(docker compose -f "$stack_dir/docker-compose.yml" ps --services)
         echo "Running services: $running_services"
 
         # Check if any services from the stack are running
         if [ -n "$running_services" ]; then
             echo "Stopping services in stack $stack_name..."
-            docker-compose -f "$stack_dir/docker-compose.yml" down
+            docker compose -f "$stack_dir/docker-compose.yml" down
         fi
 
         # Pull the latest images
         echo "Pulling latest images for stack $stack_name..."
-        docker-compose -f "$stack_dir/docker-compose.yml" pull
+        docker compose -f "$stack_dir/docker-compose.yml" pull
 
         # Start only the running services
         if [ -n "$running_services" ]; then
             echo "Starting running services in stack $stack_name..."
             for service in $running_services; do
                 echo "Starting service $service"
-                docker-compose -f "$stack_dir/docker-compose.yml" up -d "$service"
+                docker compose -f "$stack_dir/docker-compose.yml" up -d "$service"
             done
         fi
     else
